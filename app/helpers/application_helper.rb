4a9c01a219334cad3720882c23bde7af728d006f
Bayu K <50491719+bayukembara@users.noreply.github.com>
2022-05-23 22:26:44 +0700

    show detail user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def gravatar_for(user, options = { size: 80 })
+    email_address = user.email.downcase
+    hash = Digest::MD5.hexdigest(email_address)
+    size = options[:size]
+    gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+    # return it from method and alt for alternate
+    image_tag(gravatar_url, alt: user.username, class: 'rounded mx-auto shadow d-block')
+  end
 end
