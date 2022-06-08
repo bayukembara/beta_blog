671c4dd434de904bc230c157c3f6c5763bb03070
Bayu K <bayukembara@gmail.com>
2022-06-08 17:26:23 +0700

    User edit,detail and layouting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update destroy]
 
-  def index; end
+  def index
+  @users = User.all
+  end
 
   def show; end
 
