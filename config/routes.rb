4e9c44342028af42942f145e67f09e9c62654fbb
Bayu K <bayukembara@gmail.com>
2022-08-07 00:03:22 +0700

    Add new register and login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-  get 'articles/index'
-  get 'articles/show'
-  get 'articles/new'
-  get 'articles/edit'
-  get 'articles/create'
-  get 'articles/update'
-  get 'article/index'
+  get "articles/index"
+  get "articles/show"
+  get "articles/new"
+  get "articles/edit"
+  get "articles/create"
+  get "articles/update"
+  get "article/index"
   root "pages#home"
-  get 'about',to: 'pages#about'
+  get "about", to: "pages#about"
   resources :articles
-  get 'signup',to: 'users#new'
+  get "signup", to: "users#new"
   resources :users
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "login", to: "sessions#destroy"
 end
